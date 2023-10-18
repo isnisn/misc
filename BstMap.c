bc523542b3688e9c2d6f269c5eddde01bc2b932e
isn <an224qi@student.lnu.se>
2023-10-18 22:15:54 +0200

    BstMap insert and print done

--- BstMap.c
+++ BstMap.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 /** BstMap **/
 
@@ -23,11 +24,20 @@ Node_t* create_node(void* key, void* value) {
     node->key = key;
     node->value = value;
 
+    // Return a pointer to a Node_t struct
     return node;
 }
 
-void print_tree(Node_t *root) {
+void print_tree(Node_t *curr_node) {
+    if (curr_node->left != NULL) {
+        print_tree(curr_node->left);
+    }
     
+    printf("%d \n", (int)(uintptr_t)curr_node->key);
+
+    if (curr_node->right != NULL) {
+        print_tree(curr_node->right);
+    }
 }
 
 // We must also pass the node, tbi
@@ -36,16 +46,30 @@ void recursive_insert(Node_t *curr_node, void* key, void* val) {
     if(key < curr_node->key) {
         printf("Go left\n");
         // Check if left exist, if it doesnt, create it!
-        //recursive_insert(curr_node->left, key, val);
+        if(!curr_node->left) {
+            printf("Left node on current node exist, creating \n");
+            curr_node->left = create_node(key, val);
+        } else {
+            //Node does exist, traverse.
+            recursive_insert(curr_node->left, key, val);
+        }
     }
 
+    // Duplicate or key found.
     if(key == curr_node->key) {
         printf("Duplicate\n");
     }
 
     if(key > curr_node->key) {
         printf("Go Right\n");
-        //recursive_insert(curr_node->right, key, val);
+
+        if(!curr_node->right) {
+            printf("Right node on current node exist, creating \n");
+            curr_node->right = create_node(key, val);
+        } else {
+            //Node does exist, traverse.
+            recursive_insert(curr_node->right, key, val);
+        }
     }
 
 }
@@ -55,10 +79,16 @@ int main() {
     BstMap *map = malloc(sizeof(BstMap));
     
     // Create the root node
-    map->root = create_node((void*)"Wow",(void*)2);
+    map->root = create_node((void*)40,(void*)2);
 
     // Pass pointer to root-node
-    recursive_insert(map->root, (void*)"Hej",(void*)2);
+
+    recursive_insert(map->root, (void*)20,(void*)2);
+    recursive_insert(map->root, (void*)25,(void*)2);
+    recursive_insert(map->root, (void*)50,(void*)2);
+    recursive_insert(map->root, (void*)10,(void*)2);
+
+    print_tree(map->root);
 
     return 0;
 }
