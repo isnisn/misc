90c7a01334940fbdd98bca90295357cccfc3e4ed
isn <an224qi@student.lnu.se>
2023-10-18 21:34:54 +0200

    BstMap updates

--- BstMap.c
+++ BstMap.c
@@ -3,12 +3,11 @@
 
 /** BstMap **/
 
-void insert();
+void recursive_insert();
 void* get();
-Node_t* create_node();
 
 typedef struct Node_t {
-    void* key; // void* since we dont know what type key is
+    void* key; // void pointer since we dont know what type key is
     void* value; // Same here
     struct Node_t *left;
     struct Node_t *right;
@@ -28,16 +27,34 @@ Node_t* create_node(void* key, void* value) {
 }
 
 void print_tree() {
-
+    // tbi
 }
 
-void insert() {
-    
+// We must also pass the node, tbi
+void recursive_insert(Node_t *curr_node, void* key, void* val) {
+
+    if(key < curr_node->key) {
+        printf("Go left\n");
+        // Check if left exist, if it doesnt, create it!
+        recursive_insert(curr_node->left, key, val);
+    }
+
+    if(key == curr_node->key) {
+        printf("Duplicate\n");
+    }
+
+    if(key > curr_node->key) {
+        printf("Go Right\n");
+        recursive_insert(curr_node->right, key, val);
+    }
+
 }
 
 int main() {
     BstMap *map = malloc(sizeof(BstMap));
     map->root = create_node((void*)1,(void*)2);
+    // Pass pointer to root-node
+    recursive_insert(map->root, (void*)2,(void*)2);
 
     return 0;
 }
